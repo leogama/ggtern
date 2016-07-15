6ec9b4ba02f450b38ba1d1dc03992a24b92ce756
Nicholas Hamilton <nicholasehamilton@gmail.com>
2016-07-16 09:49:40 +1000

    Created getLabels function to better handle the creation of the ternary labels, from the known breaks. Have set the earliest label to empty (ie ""), so it doesn't interfere with the axis titles, particularly if the axis titles and first label are wide (ie something like "Top Species" and "50.%" respectively). This is not generally a problem for the default plot where the axis titles and first label are trivial (ie something like "T" and "0")

--- R/theme-mesh.R
+++ R/theme-mesh.R
@@ -1,13 +1,13 @@
 #' Create Grid Mesh
 #' 
 #' Convenience function for creation of a grid mesh of 'n' major breaks.
-#' @param number the number of major breaks.
+#' @param n the number of major breaks.
 #' @export
-theme_mesh = function(number = 5){
-  if(!is.numeric(number)) 
-    stop("'number' must be numeric")
-  limits = c(0,1)
-  number = max(as.integer(number[1]),1)
-  limit_tern(breaks       = getBreaks(limits = limits, isMajor = T, nMajor = number), 
-             minor_breaks = getBreaks(limits = limits, isMajor = F, nMajor = number))
+theme_mesh = function(n = 5){
+  if(!is.numeric(n)) 
+    stop("'n' must be numeric")
+  l = c(0,1)
+  n = max(as.integer(n[1]),1)
+  limit_tern(breaks       = getBreaks(limits = l, isMajor = T, n = n), 
+             minor_breaks = getBreaks(limits = l, isMajor = F, n = n))
 }
