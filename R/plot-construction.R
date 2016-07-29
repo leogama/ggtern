583760735f16e2701ce5fc1c4d018828f2083149
Nicholas Hamilton <nicholasehamilton@gmail.com>
2016-07-29 23:13:21 +1000

    Brought forward semi-local masked copy of add_ggplot, for the local handling of the update_theme function

--- R/plot-construction.R
+++ R/plot-construction.R
@@ -10,5 +10,17 @@
   # can be displayed in error messages
   e2name <- deparse(substitute(e2))
   if      (is.theme(e1))  add_theme( e1, e2, e2name)
-  else if (is.ggplot(e1)) ggint$add_ggplot(e1, e2, e2name)
+  else if (is.ggplot(e1)) add_ggplot(e1, e2, e2name)
+}
+
+add_ggplot <- function(p, object, objectname) {
+  if (is.null(object)) return(p)
+  if (is.theme(object)) {
+    p <- ggint$plot_clone(p)
+    p$theme <- update_theme(p$theme, object)
+    ggint$set_last_plot(p)
+  }else{
+    p = ggint$add_ggplot(p,object,objectname)
+  }
+  p
 }
